b84f7a52797b0e92255fe2a9af34b8a847e1c447
davidburlaka <62345662+davidburlaka@users.noreply.github.com>
2020-04-06 00:14:04 +0300

    Updated home page

---
+++ IPZ_Project/src/views/Components.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <hero></hero>
+        <TopicList></TopicList>
+    </div>
+</template>
+<script>
+import Hero from "./components/Hero";
+import TopicList from "./components/TopicList";
+
+
+export default {
+  name: "components",
+  components: {
+      Hero,
+      TopicList
+  }
+};
+</script>
---
+++ IPZ_Project/src/views/components/Hero.vue
@@ -0,0 +1,36 @@
+﻿<template>
+    <section class="section-hero section-shaped my-0 ">
+        <div class="shape shape-style-1 shape-primary">
+            <span class="span-150"></span>
+            <span class="span-50"></span>
+            <span class="span-50"></span>
+            <span class="span-75"></span>
+            <span class="span-100"></span>
+            <span class="span-75"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+        </div>
+        <div class="container shape-container d-flex align-items-center">
+            <div class="col px-0">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-lg-7 text-center pt-lg">
+                        <img src="img/brand/white.png" style="width: 400px;" class="img-fluid">
+                        <p class="lead text-white mt-4 mb-5">Сучасний онлайн курс по JS. Навчись програмувати вже сьогодні!</p>
+                        <!-- Search form -->
+                        <div class="active-purple-4 mb-4">
+                            <input class="form-control" type="text" placeholder="Пошук" aria-label="Search">
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
